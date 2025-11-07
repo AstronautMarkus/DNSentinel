deff384456b3e701787a167b5dbeb2bd8348804f
Marcos Reyes <astronautmarus@gmail.com>
2025-11-07 16:52:34 -0300

    feat: added Manage ISP IP and Ethernet IP in dashboard home page.

--- dnsentinnel/app/routes/dashboard/home.py
+++ dnsentinnel/app/routes/dashboard/home.py
@@ -1,6 +1,21 @@
 from flask import render_template
 from app.routes.dashboard import dashboard_bp
+import requests
+import socket
+
 
 @dashboard_bp.route('/dashboard/home')
 def home():
-    return render_template('dashboard/home.html')
+
+    isp_ip = requests.get('https://api.ipify.org').text
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(('8.8.8.8', 80))
+        ethernet_ip = s.getsockname()[0]
+    except Exception:
+        ethernet_ip = '127.0.0.1'
+    finally:
+        s.close()
+
+    return render_template('dashboard/home.html', isp_ip=isp_ip, ethernet_ip=ethernet_ip)
