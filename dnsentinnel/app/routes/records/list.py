c1a4945198e262fc1a2d1413516b37ded33c943c
Marcos Reyes <astronautmarkus@gmail.com>
2025-11-09 02:07:58 -0300

    feat: add zone name header to DNS record and zone lists for improved context

--- dnsentinnel/app/routes/records/list.py
+++ dnsentinnel/app/routes/records/list.py
@@ -11,4 +11,4 @@ def list_records(zone_id):
     records = []
     if zone:
         records = Record.query.filter_by(zone_id_fk=zone.id).all()
-    return render_template('records/list.html', zone_id=zone_id, records=records)
+    return render_template('records/list.html', records=records, zone=zone)
