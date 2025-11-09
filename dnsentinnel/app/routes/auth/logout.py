0764fbdca1b76c84526c053d74d9506d3d727073
Marcos Reyes <astronautmarkus@gmail.com>
2025-11-09 00:06:50 -0300

    refactor: streamline logout process by removing session handling and utilizing Flask-Login for user logout

--- dnsentinnel/app/routes/auth/logout.py
+++ dnsentinnel/app/routes/auth/logout.py
@@ -1,9 +1,10 @@
-from flask import redirect, url_for, flash, session
+from flask import redirect, url_for, flash
+from flask_login import logout_user, current_user
 from app.routes.auth import auth_bp
 
 @auth_bp.route('/logout')
 def logout():
-    user_name = session['user_name']
-    session.clear()
+    user_name = current_user.name if current_user.is_authenticated else "user"
+    logout_user()
     flash(f'See you, {user_name}! You have been logged out. We\'ll continue working in the background to keep your addresses updated!', 'info')
     return redirect(url_for('auth.login'))
